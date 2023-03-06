dfc3c32452640b85c1ce86320b7aaf7fd81fd8bb
Red <red.sarouf2014@gmail.com>
2023-03-06 11:27:13 -0500

    Added Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 15.50;
+let num2 = 4
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+let max = Math.max(2,3,4,7,12,20);
